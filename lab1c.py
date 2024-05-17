f633bcdcc03472dead081acff76e67a72bdd933a
samir <sthapa23@myseneca.ca>
2024-05-17 18:26:16 -0400

    this is part 3 for lab 1

---
+++ lab1c.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''Description: this is samir thapa and this is part c from samir thapa'''
+
+# num1 = 5
+# num2 = 10
+# print(num1)
+# print(num2)       #practice
+# sum = num1 + num2
+# print(sum)
+# print('The sum is: ' + str(sum))
+
+name = 'Isaac'
+age = 72
+print(name + ' is ' + str(age) + ' years old!')
