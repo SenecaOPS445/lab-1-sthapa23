7d6d828f2f758ee4b9807c16d1c63fdf52361031
samir <sthapa23@myseneca.ca>
2024-05-17 18:57:05 -0400

    this is the part d lab 1 from samir thapa

---
+++ lab1d.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''this is samir thapa doing the part d from lab1'''
+
+# print(10 + 5)
+# print(10 - 5)
+# print(10 * 5)       #just practice and testing the calculation
+# print(10 / 5)
+# print(10 ** 5)
+# print(10 + 5 * 2)
+# print((10 + 5) * 2)
+# print(10 + 5 * 2 - 10 ** 2)
+# print(15 / 3 * 4)
+# print(100 / ((5 + 5) * 2))
+
+x = 10
+y = 2
+z = 5
+a = str(x + y * z)
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(a))
